841abe545e93e8515d13f5d9b591a0b5020974eb
aevtikheev <eft000@gmail.com>
2021-03-25 22:17:09 +0300

    Set up autoreload for celery worker

---
+++ polls/management/commands/celery_worker.py
@@ -0,0 +1,16 @@
+import shlex
+import subprocess
+
+from django.core.management.base import BaseCommand
+from django.utils import autoreload
+
+
+def restart_celery():
+    subprocess.call(shlex.split('pkill -f "celery worker"'))
+    subprocess.call(shlex.split('celery worker -A django_celery_example --loglevel=info'))
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        print('Starting celery worker with autoreload...')
+        autoreload.run_with_reloader(restart_celery)
